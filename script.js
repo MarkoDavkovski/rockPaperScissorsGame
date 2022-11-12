f0d45167983cc47bebb5177b534996e122b9bc2a
Marko Davkovski <marko.davkovski@gmail.com>
2022-11-12 14:14:20 +0100

    JS DOM interaction

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
+let btnSign = document.querySelectorAll('.btnSign');
+let playerScore = document.querySelector('.playerScore');
+let computerScore = document.querySelector('.computerScore');
+let computerSign = document.querySelector('.computerSign');
+let playerSign = document.querySelector('.playerSign');
+
 const signs = ['rock', 'paper', 'scissors'];
 let computerPoints = 0;
 let playerPoints = 0;
+let playerSelection;
+let computerSelection;
+for (btn of btnSign) {
+	btn.addEventListener('click', (e) => {
+		computerSelection = getComputerChoice();
+		playerSelection = e.target.id;
+		playRound(playerSelection, computerSelection);
+		playerScore.textContent = `Player: ${playerPoints}`;
+		computerScore.textContent = `Computer: ${computerPoints}`;
+		computerSign.textContent = `${getComputerSign(computerSelection)}`;
+		playerSign.textContent = `${getPlayerSign(playerSelection)}`;
+
+		if (computerPoints === 5) {
+			alert('Better luck next time');
+			restartGame();
+		} else if (playerPoints === 5) {
+			alert('Congratulations!');
+			restartGame();
+		}
+	});
+}
+
+function getComputerSign(computerSelection) {
+	if (computerSelection === 'rock') return '✊🏼';
+	else if (computerSelection === 'paper') return '✋🏼';
+	else if (computerSelection === 'scissors') return '✌🏼';
+}
+function getPlayerSign(playerSelection) {
+	if (playerSelection === 'rock') return '✊🏼';
+	else if (playerSelection === 'paper') return '✋🏼';
+	else if (playerSelection === 'scissors') return '✌🏼';
+}
+
 function getComputerChoice() {
 	const randomIndex = Math.floor(Math.random() * signs.length);
 	return signs[randomIndex];
 }
 
 function playRound(playerSelection, computerSelection) {
-	playerSelection.toLowerCase();
-	computerSelection.toLowerCase();
 	let tempComputer = computerPoints;
 	let tempPlayer = playerPoints;
 	const msg = `It's a tie!`;
@@ -28,23 +65,12 @@ function playRound(playerSelection, computerSelection) {
 	if (tempComputer < computerPoints) return 'Computer wins the round';
 	else if (tempPlayer < playerPoints) return 'Player wins the round';
 }
-let playerSelection;
-let computerSelection;
 
-function game() {
-	for (let i = 0; i < 5; i++) {
-		playerSelection = prompt(
-			'Write down your selection between rock, paper and scissors!'
-		);
-		computerSelection = getComputerChoice();
-		playRound(playerSelection, computerSelection);
-		console.log(`Player :${playerSelection} Computer : ${computerSelection}`);
-		console.log(`Player :${playerPoints} Computer : ${computerPoints}`);
-		if (playerSelection === computerSelection) i--;
-	}
-
-	if (computerPoints < playerPoints) return 'Congratulations, you win!';
-	else return 'Better luck next time!';
+function restartGame() {
+	computerPoints = 0;
+	playerPoints = 0;
+	playerScore.textContent = `Player: ${playerPoints}`;
+	computerScore.textContent = `Computer${computerPoints}`;
+	computerSign.textContent = `❔`;
+	playerSign.textContent = `❔`;
 }
-
-console.log(game());
